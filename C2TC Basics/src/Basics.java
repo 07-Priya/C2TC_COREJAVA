c90d98a8e8e28f3c289f1f4236715723173286c6
priya <priya.r.acharya.07@gmail.com>
2022-11-26 12:21:36 +0530

    first commit

---
+++ C2TC Basics/src/Basics.java
@@ -0,0 +1,19 @@
+
+public class Basics {
+
+	public static void main(String[] args) {
+		int outer=2;
+		{
+			int inner=1;
+			System.out.println(inner);
+			System.out.println(outer);
+		}
+		int inner=3;
+		System.out.println(inner);
+		
+
+	}
+
+}
+
+
