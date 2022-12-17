8f436ed064d238c9be353cfa1f20f316d950444d
priya <priya.r.acharya.07@gmail.com>
2022-12-17 09:47:46 +0530

    good

---
+++ Polymorphism/src/Adder.java
@@ -0,0 +1,20 @@
+
+class Adder{
+
+ static int add(int a,int b)
+
+{
+
+return a+b;
+
+}
+
+static double add1(int a,int b)
+
+{
+
+return a+b;
+
+}
+
+}
---
+++ Polymorphism/src/TestOverloading3.java
@@ -0,0 +1,14 @@
+
+class TestOverloading3
+
+{
+
+public static void main(String[] args)
+
+{
+
+System.out.println(Adder.add(11,11));
+
+}
+
+}
